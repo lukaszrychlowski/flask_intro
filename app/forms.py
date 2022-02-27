30b9100574997f7ae63ef137c7647ced3e451f44
lukaszrychlowski <rychlowski@agh.edu.pl>
2022-02-27 21:33:26 +0100

    login added

---
+++ app/forms.py
@@ -0,0 +1,11 @@
+from io import StringIO
+from wsgiref import validate
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    username = StringField('user', validators=[DataRequired()])
+    password = StringField('password', validators=[DataRequired()])
+    remember_me = BooleanField('remember me')
+    submit = SubmitField('Sign In')
