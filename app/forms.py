4beeec59b90e80db3888594112440cf6a826e93a
lukaszrychlowski <rychlowski@agh.edu.pl>
2022-03-08 17:59:22 +0100

    working on user profiles

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,28 @@
+from errno import EDESTADDRREQ
+from tokenize import String
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     username = StringField('user', validators=[DataRequired()])
     password = StringField('password', validators=[DataRequired()])
     remember_me = BooleanField('remember me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('username', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email()])
+    password = StringField('password', validators=[DataRequired()])
+    password2 = StringField('repeat pw', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('register')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('username already taken')
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('email address already taken')
