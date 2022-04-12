9223ffe59e450f9d5144a281062e5e640f286b6f
lukaszrychlowski <rychlowski@agh.edu.pl>
2022-04-12 15:06:02 +0200

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -4,14 +4,17 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 mail = Mail(app)
+bootstrap = Bootstrap(app)
 
 login = LoginManager(app)
 login.login_view = 'login'
 
-from app import routes, models
+
+from app import routes, models, errors
